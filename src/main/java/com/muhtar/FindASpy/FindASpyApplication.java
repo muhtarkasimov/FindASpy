854ea381e5cee493e3df5d5950cbb91c3f1fc94c
muhtarkasimov <muhtarkasimov@gmail.com>
2020-06-07 20:26:41 +0600

    Initial Commit

---
+++ src/main/java/com/muhtar/FindASpy/FindASpyApplication.java
@@ -0,0 +1,13 @@
+package com.muhtar.FindASpy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindASpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindASpyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muhtar/FindASpy/FindASpyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhtar.FindASpy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindASpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
