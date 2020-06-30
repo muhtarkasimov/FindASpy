3effe4a26013b64387b524fa89bd9a9295c44ab2
muhtarkasimov <muhtarkasimov@gmail.com>
2020-06-30 08:06:34 +0600

    Наконец передал данные в GameController.

--- src/main/java/com/muhtar/FindASpy/controller/GameController.java
+++ src/main/java/com/muhtar/FindASpy/controller/GameController.java
@@ -2,18 +2,17 @@ package com.muhtar.FindASpy.controller;
 
 import com.muhtar.FindASpy.entity.User;
 import com.muhtar.FindASpy.model.Game;
-import com.muhtar.FindASpy.model.Room;
 import com.muhtar.FindASpy.service.GamesPool;
 import com.muhtar.FindASpy.service.RoomsPool;
 import com.muhtar.FindASpy.service.UserService;
 import com.muhtar.FindASpy.service.WordsPool;
-import com.sun.istack.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -38,35 +37,38 @@ public class GameController {
                            HttpServletRequest request
     ) {
         model.addAttribute("game", (Game)model.getAttribute("game"));
-//        gamePool.getGameById(gameId).getUsers().add(
-//                userService.getByUsername(request.getUserPrincipal().getName())
-//        );
+        model.addAttribute("gameId", gameId);
+
         return "gameRoom";
     }
 
     @PostMapping
     public String createGame(@ModelAttribute Game game,
-                             Model model,
-                             HttpServletRequest request
+                             @ModelAttribute(name = "roomId") String roomId,
+                             Model model
     ) {
+        ArrayList<User> players = roomsPool.getRoomByStringId(roomId).getUsers();
+        System.err.println("RoomId : " + roomId);
+        System.err.println("Players : " + players);
         Game newGame = Game.builder()
-//                .players((List<User>)game.getPlayers())
+                .players(players)
                 .gameTime(game.getGameTime())
                 .isActive(true)
-//                .spiesAmount(game.getSpiesAmount())
+                .spiesAmount(game.getSpiesAmount())
                 .word(words.getRandomWord())
                 .build();
         newGame.selectSpies();
 
-        String gameId = gamesPool.addGame(newGame);
+        String gameId = gamesPool.addGame(roomId ,newGame);
 
         //Внимание: Вот тут рассылать редирект всем игроком на
         // автотический переход в игру
         // ...
 
 //        newGame.startGame();
-//        model.addAttribute("game", newGame);
-//        model.addAttribute("gameId", gameId);
+        model.addAttribute("game", newGame);
+        model.addAttribute("gameId", gameId);
+
         return "redirect:/games/" + gameId;
     }
 
